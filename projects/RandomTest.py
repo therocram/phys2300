d46fdcd977c6583466a442924739556daca1b5b7
Masen Pitts <masenpitts@ad.weber.edu>
2021-11-03 16:23:37 -0600

    Started Random Processes Projects

---
+++ projects/RandomTest.py
@@ -0,0 +1,18 @@
+# RandomTest
+# made by Masen Pitts and Bracken Phelps
+# Updated 11/3/2021
+
+# Import necessary packages
+from math import sqrt, exp, factorial
+from random import random
+import matplotlib.pyplot as plt
+
+# Print a list of pseudo-random numbers ranging from 0-1
+for i in range(20):
+    print(random())
+
+print("\n\n")
+
+# Print a list of pseudo-random integers ranging from 0-9
+for i in range(50):
+    print(int(10*random()))
