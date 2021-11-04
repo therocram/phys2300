24b33509d3678ae567a62c326c32bb6ad929eef9
unknown <therocram@gmail.com>
2021-11-04 17:16:41 -0600

    Modified RandomTest program

--- projects/RandomTest.py
+++ projects/RandomTest.py
@@ -16,3 +16,21 @@ print("\n\n")
 # Print a list of pseudo-random integers ranging from 0-9
 for i in range(50):
     print(int(10*random()))
+
+# Plot a certain number of points on an xy-plane with pseudo-random x and y
+# values ranging from 0 to a specified maximum
+#******************************************************************************
+points = 1000 # Determines the number of points in the plot
+max = 10 # Determines the upper bound for the pseudo-random values
+x = [0]*points # Lists that store the x and y positions of the points to be plotted
+y = [0]*points #
+
+# Assigns pseudo-random x and y values
+for i in range(points):
+    x[i] = max*random()
+    y[i] = max*random()
+    
+# Plot the points
+plt.plot(x, y, marker="+", markersize=5, color="red", linestyle="None")
+plt.show()
+#******************************************************************************
