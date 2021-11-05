54548b46c89e2888ffae313983833815ae46e3d9
Masen Pitts <masenpitts@ad.weber.edu>
2021-11-05 16:22:29 -0600

    Finished RandomTest and started TwoBoxes

--- projects/RandomTest.py
+++ projects/RandomTest.py
@@ -1,6 +1,8 @@
 # RandomTest
+# Simple program created to perform non-exhaustive tests on just how
+# random the "random()" function in Python is.
 # made by Masen Pitts and Bracken Phelps
-# Updated 11/3/2021
+# Updated 11/5/2021
 
 # Import necessary packages
 from math import sqrt, exp, factorial
@@ -21,14 +23,14 @@ for i in range(50):
 # values ranging from 0 to a specified maximum
 #******************************************************************************
 points = 1000 # Determines the number of points in the plot
-max = 10 # Determines the upper bound for the pseudo-random values
+maxValue = 10 # Determines the upper bound for the pseudo-random values
 x = [0]*points # Lists that store the x and y positions of the points to be plotted
 y = [0]*points #
 
 # Assigns pseudo-random x and y values
 for i in range(points):
-    x[i] = max*random()
-    y[i] = max*random()
+    x[i] = maxValue*random()
+    y[i] = maxValue*random()
     
 # Plot the points
 plt.plot(x, y, marker="+", markersize=5, color="red", linestyle="None")
---
+++ projects/TwoBoxes.py
@@ -0,0 +1,47 @@
+# TwoBoxes
+# Simple program using Monte-Carlo methods to simulates two boxes
+# of gas molecules seperated by a partition.
+# made by Masen Pitts and Bracken Phelps
+# Updated 11/5/2021
+
+# Import necessary packages
+from math import sqrt, exp, factorial
+from random import random
+import matplotlib.pyplot as plt
+
+n = 1000 # Total number of gas molecules
+nLeft = int(n/2) # Total number of gas molecules in the left-hand box;
+                 # all molecules start out in the left-hand box
+             
+duration = 10000 # Determines the number of "time" steps that the simulation runs
+tList = range(duration) # List used to plot the "time" axis of the plot
+nLeftList = [0]*duration # List used to plot the number of molecules in the left-hand box vs. "time"
+hist = [0]*(n+1) # List used to plot a histogram of all all possible values of nLeft
+
+# Randomly selects a gas molecule and moves it to the opposite box
+for i in range(duration):
+    # Calculate a random number between 0 and n. If this number is less than nLeft
+    # then one of the molecules in the left-hand box is moved to the right-hand box. Otherwise
+    # a molecule moves from the right-hand box to the left-hand box.
+    nLeftList[i] = nLeft
+    hist[nLeft] += 1
+    if n*random() < nLeft:
+        nLeft -= 1
+    else:
+        nLeft += 1
+    
+'''
+# Plot the number of molecules in the left-hand box vs. "time"
+plt.plot(tList, nLeftList, marker="+", markersize=5, color="red", lineStyle="None")
+plt.title("Number of Molecules in Left Side of the Box wrt \"Time\"")
+plt.grid()
+plt.xlim(0, duration)
+plt.ylim(0, n)
+plt.xticks(range(0, duration + 1, int(duration/10)))
+plt.yticks(range(0, n + 1, int(n/10)))
+plt.xlabel("\"Time\"")
+plt.ylabel("Number of Molecules in Left Side")
+plt.show()
+'''
+
+
