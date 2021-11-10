b5e539e36f8d440b3a8bf7a80a6e35418c374272
therocram <therocram@gmail.com>
2021-11-10 14:03:22 -0700

    Added RMS calculation to RandomWalk

--- projects/RandomWalk.py
+++ projects/RandomWalk.py
@@ -9,8 +9,11 @@ from math import sqrt, exp, factorial
 from random import random
 import matplotlib.pyplot as plt
 
-duration = 1000 # Total number of "time" steps that the simulation runs
+duration = 1000 # Total number of "time" steps that the simulation runs for each "walker"
 tList = range(duration) # List used to plot the "time" axis of the postition vs. "time" graph
+nWalkers = 20 # Determines the number of "walkers" that the program will simulate
+totalDistanceSquared = 0 # Variable used to keep track of the sum of the squares of the net distances
+                         # of each of the "walkers"
 
 plt.title("Particle X-Position With Respect to Time")
 plt.xlabel("\"Time\"")
@@ -19,8 +22,9 @@ plt.grid()
 plt.xticks(range(0, duration + 1, int(duration/10)))
 plt.xlim(0, duration)
 
-# Simulate the random walk motion a certain number of times and graph them all together
-for i in range(20):
+# Simulate the random walk motion for a certain number of "walkers" and graph their positions as a function
+# of "time"
+for i in range(nWalkers):
     x = 0 # Position of particle
     xPos = [0]*duration # List used to plot the positions of the particle wrt "time"
     
@@ -34,7 +38,11 @@ for i in range(20):
             x += 1
         else:
             x -= 1
-    
+    totalDistanceSquared += (xPos[duration-1])**2 # Add the square of the final position of the current 
+                                                  # "walker" to the total
     plt.plot(tList, xPos, color="red")
+
+rms = sqrt(totalDistanceSquared/nWalkers) # Calculate the root-mean-square net distance traveled by the "walkers"
+print(rms)
 
 plt.show()    
