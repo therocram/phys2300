dd2e9e88c888ea529ae1a0354980f12242660c57
Masen Pitts <masenpitts@ad.weber.edu>
2021-11-10 16:24:13 -0700

    Finished RandomWalk and Decay

--- projects/Decay.py
+++ projects/Decay.py
@@ -9,8 +9,30 @@ from math import sqrt, exp, factorial
 from random import random
 import matplotlib.pyplot as plt
 
-n = 100 # Total number of radioactive nuclei in isotope sample
+n = 10000 # Total number of radioactive nuclei in isotope sample
 nCurrent = n # Keeps track of the number of remaining nuclei
-duration = 10000 # Total number of "time" steps that the simulation runs
+duration = 8000 # Total number of "time" steps that the simulation runs
 
-nList = [0]*duration # List plot the number of remaining nuclei vs. "time"
+nList = [0]*(duration+1) # List used to plot the number of remaining nuclei vs. "time"
+
+# Simulate random radioactive decay of the nuclei. Probability of decay is
+# 0.001 for each nucleus
+for i in range(duration+1):
+    nList[i] = nCurrent
+    # For each remaining atom calculate a random number between 0 and 1 (including 0). 
+    # If this number is less than 0.001 decrease the number of remaining nuclei by 1.
+    # Otherwise nothing happens.
+    for j in range(nCurrent):
+        if random() < 0.001:
+            nCurrent -= 1
+    
+# Plot number of remaining nuclei in sample vs. "time"
+plt.plot(range(duration+1), nList, color="green")
+plt.xticks(range(0, duration + 1, int(duration/10)))
+plt.xlim(0, duration)
+plt.ylim(0, n)
+plt.grid()
+plt.title("Number of Remaining Nuclei in the Sample With Respect to \"Time\"")
+plt.xlabel("\"Time\"")
+plt.ylabel("Number of Remaining Nuclei")
+plt.show()
--- projects/RandomWalk.py
+++ projects/RandomWalk.py
@@ -2,20 +2,21 @@
 # Simple program using Monte-Carlo methods to simulate random-walk motion
 # in one dimension.
 # made by Masen Pitts and Bracken Phelps
-# Updated 11/8/2021
+# Updated 11/10/2021
 
 # Import necessary packages
 from math import sqrt, exp, factorial
 from random import random
 import matplotlib.pyplot as plt
 
-duration = 1000 # Total number of "time" steps that the simulation runs for each "walker"
-tList = range(duration) # List used to plot the "time" axis of the postition vs. "time" graph
+duration = 100000 # Total number of "time" steps that the simulation runs for each "walker"
+tList = range(duration+1) # List used to plot the "time" axis of the postition vs. "time" graph
 nWalkers = 20 # Determines the number of "walkers" that the program will simulate
 totalDistanceSquared = 0 # Variable used to keep track of the sum of the squares of the net distances
                          # of each of the "walkers"
 
-plt.title("Particle X-Position With Respect to Time")
+# Set attributes and labels for graph
+plt.title("X-Position of Random Walkers With Respect to \"Time\"")
 plt.xlabel("\"Time\"")
 plt.ylabel("X")
 plt.grid()
@@ -26,10 +27,10 @@ plt.xlim(0, duration)
 # of "time"
 for i in range(nWalkers):
     x = 0 # Position of particle
-    xPos = [0]*duration # List used to plot the positions of the particle wrt "time"
+    xPos = [0]*(duration+1) # List used to plot the positions of the particle wrt "time"
     
     # Moves the particle in random direction
-    for j in range(duration):
+    for j in range(duration+1):
         # Calculate a random number between 0 and 1 (including 0). If this number is less than 0.5
         # move the particle one unit in the positive x-direction. Otherwise move the particle one unit 
         # in the negative x-direction
@@ -38,11 +39,11 @@ for i in range(nWalkers):
             x += 1
         else:
             x -= 1
-    totalDistanceSquared += (xPos[duration-1])**2 # Add the square of the final position of the current 
-                                                  # "walker" to the total
-    plt.plot(tList, xPos, color="red")
+    totalDistanceSquared += (xPos[duration])**2 # Add the square of the final position of the current 
+                                                # "walker" to the total
+    plt.plot(tList, xPos, color="red")  # Graph the x-position of the current "walker" wrt time
 
 rms = sqrt(totalDistanceSquared/nWalkers) # Calculate the root-mean-square net distance traveled by the "walkers"
-print(rms)
+print(rms)                                # and print the value to screen
 
 plt.show()    
